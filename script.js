a8d8da69cae44dab79588a7ed906406de13bc53e
Bedatop <98839707+Bedatop@users.noreply.github.com>
2023-03-31 14:51:42 +0500

    правки

--- script.js
+++ script.js
@@ -6,4 +6,19 @@ add.addEventListener('click', function () {
     let li = document.createElement('li');
     li.innerHTML = inp.value;
     ul.appendChild(li);
+    inp.value = '';
+    let check = document.createElement('input');
+    check.type = 'checkbox'
+    li.appendChild(check);
+    check.addEventListener('click', function () {
+        li.classList.toggle('add');
+    })
+    let del = document.createElement('a');
+    del.href = ''
+    del.innerHTML ='X';
+    li.appendChild(del);
+    del.addEventListener('click', function (ev) {
+        ev.preventDefault();
+        li.parentElement.removeChild(li)
+    })
 })
